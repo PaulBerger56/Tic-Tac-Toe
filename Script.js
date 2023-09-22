f1945f0e530a8a7e233d456755e2be938f0f5d8a
Paul Berger <paulberger56@gmail.com>
2023-09-21 22:24:33 -0400

    Create the gameboard in JS

--- Script.js
+++ Script.js
@@ -1,4 +1,22 @@
-const gameBoard = (() => {
-    const board = document.querySelector('.gameboard')
+const GameBoard = (() => {
+    let gameboard = ["", "", "", "", "", "", "", "", ""] 
+
+    const render  = () => {
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id=square-${index}">${square}</div>`
+        })
+    }
+    document.querySelector("#gameboard").innerHTML = boardHTML;
+
+    return {
+        render,
+    }
 })();
+
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", ()=> {
+//    Game.start();
+})
+
 
