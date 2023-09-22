ec15f65b337d43628a87d2e1893d395fe66abf57
Paul Berger <paulberger56@gmail.com>
2023-09-21 23:36:24 -0400

    Add the displayController and have it update the win and tie messages

--- Script.js
+++ Script.js
@@ -1,3 +1,13 @@
+const displayController = (() => {
+    const renderMessage = (message) => {
+        document.querySelector("#message").innerHTML = message;
+    }
+
+    return {
+        renderMessage,
+    }
+})();
+
 const Gameboard = (() => {
     let gameboard = ["", "", "", "", "", "", "", "", ""] 
 
@@ -51,11 +61,23 @@ const Game = (() => {
     }
 
     const handleClick = (event) => {
+        if(gameOver) {
+            return;
+        }
         let index = parseInt(event.target.id.split("-")[1]);
         if(Gameboard.getGameboard()[index] !=="")
             return;
 
-        Gameboard.update(index, players[currentPlayerIndex].mark);     
+        Gameboard.update(index, players[currentPlayerIndex].mark);  
+        
+        if(checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].mark)) {
+            gameOver = true;
+            displayController.renderMessage(`${players[currentPlayerIndex].name} wins!`)
+        } else if (checkForTie(Gameboard.getGameboard())) {
+            gameOver = true;
+            displayController.renderMessage("It's a tie!");
+        }
+        
         currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
     }
 
@@ -74,6 +96,31 @@ const Game = (() => {
     }
     
 })();
+
+function checkForWin(board) {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for ( let i = 0; i < winningCombinations.length; i++) {
+        const [a, b, c] = winningCombinations[i];
+        if(board[a] && board[a] === board[b] && board[a] === board[c]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkForTie(board) {
+    return board.every(cell => cell !== "")
+}
 
 const restartButton = document.querySelector("#restart-button");
 restartButton.addEventListener("click", () => {
